157016a08a433ad881251d7af598fe2504b90386
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-17 16:46:58 -0700

    fix1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main (void){
-	char Mystr[] = "Programming is like building a multilingual puzzle. \n";
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle \n";
+
 	puts(Mystr);
-	return (0);}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+//	int number = n[-1];
+	printf("%d \n", n);
+	return (0);
+}
