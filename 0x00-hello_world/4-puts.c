d2931e88d6f1ff6a33a72ad2fa0cf12d5396063d
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-18 10:31:43 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * Main
  * return(0)
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
 
-	puts(Mystr);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
