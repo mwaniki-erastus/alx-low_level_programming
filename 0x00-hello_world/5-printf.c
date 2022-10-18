4cada5e64d0428fd4ef61bae43e0520eb7895381
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-17 17:13:33 -0700

    fix3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point during execution
- * Always return 0
+ * Always return (0)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art. \n");
+	printf("with proper grammar, but the outcome is a piece of art \n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-int main(void){
-	char i; int j; long int k; long long int l; float m; 
-	printf("The size of a char: %lu byte(s) \n",(unsigned long)sizeof(i));
-	printf("The size of an int : %lu byte(s) \n",(unsigned long)sizeof(j));
-	printf("The size of a long int: %lu byte(s) \n",(unsigned long)sizeof(k));
-	printf("The size of a long long int : %lu byte(s) \n",(unsigned long)sizeof(l));
-	printf("The size of a float: %lu byte(s) \n",(unsigned long)sizeof(m));
-	return (0);}
+/*
+ * Main
+ * Always return (0)
+ */
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of an int : %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("Size of a long long int : %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(m));
+	return (0);
+}
