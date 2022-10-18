494dc71819ceac8b87822b0d7e71cee3d327dd8f
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-18 14:38:57 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * Main
- * return(0)
+ * main- Entry point during execution
+ *
+ *Return always 0 (success)
  */
+
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
