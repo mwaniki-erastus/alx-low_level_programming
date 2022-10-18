6d4decb387683bf4bc14c10a08d8719101b36434
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-18 10:44:33 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	float m;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of an int : %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(j));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
-	printf("Size of a long long int : %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(m));
 	return (0);
 }
