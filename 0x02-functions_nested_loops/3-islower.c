43cc89c66b26439dc92e652f4ce915f28a6f8fc3
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-19 19:47:49 -0700

    slower:q

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - Prototype
+ * @c: parameter
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
