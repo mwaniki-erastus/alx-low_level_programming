b3d9b710726437b9380e01849bafc6a18b4abe02
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-19 19:39:50 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,14 @@
-#include <main.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
 /**
  * main - Entry point during execution
- *
+ * iProgram that prints all single digits of base 10 starting from 0
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	write(1, "_putchar", strlen("_putchar"));
+	putchar(10);
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,31 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
 void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ */
+
 void print_alphabet_x10(void);
+
+/**
+ * _islower - checks for lowercase
+ * @c: the character to check
+ * Return: int
+ */
+
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-int _putchar(char c);
 
-#endif
+#endif /* _MAIN_H_ */
