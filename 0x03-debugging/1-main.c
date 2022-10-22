767913a38ff7e7dd80a3ac0e9fa8cb760518bd58
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-21 18:05:40 -0700

    one

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main 0 test function for positive-negative
+ *
+ * Return: Always 0 (success)
+ */
+int main(void) 
+{
+	int i;
+
+	i=0;
+	positive_or_negative(1);
+
+	return(0);
+}
