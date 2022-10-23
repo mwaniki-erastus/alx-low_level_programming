d3c00365f27900aa10ed708364ec3505d5e25afb
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-23 02:43:19 -0700

    alphsbets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,9 @@
-#include "main.h"
-
-/**
- * print_alphabet - Prints lowercase alphabet
- *
- * Return: Always 0
- */
-
 void print_alphabet(void)
 {
-	char n;
+	char ch;
 
-	for (n = 'a'; n <= 'z'; n++)
-		_putchar(n);
-	putchar(10);
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c.copy
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints lowercase alphabet
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	putchar(10);
+}
