6389900677f30dca8a37134c187ed1cecf8917ff
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-23 02:14:23 -0700

    fix 01

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point during execution
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(1, "_putchar", strlen("_putchar"));
+	putchar(10);
+	return (0);
+}
