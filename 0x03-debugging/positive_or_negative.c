c23a12b140081272692f04194eb965965114d2b2
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-23 06:50:35 -0700

    multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+ * This program tests that the function positive_or_negative() gives
+ * the correct output when given a case of 0.
+ */
 void positive_or_negative(int i)
 {
 	if (i > 0)
