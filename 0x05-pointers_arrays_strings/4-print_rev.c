6a5c880fd92bb19823c0bf411630e5d2b61baf51
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-26 19:18:27 -0700

    Third commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Write a function that prints a string, in reverse, followed by a new line 
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
