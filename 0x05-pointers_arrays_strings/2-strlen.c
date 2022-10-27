4a4014f2e7f28a42e23fb68ac023ef2a0dd95ceb
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-26 18:56:04 -0700

    return lenth of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: return string
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int k;
+
+	for (k = 0; s[k] != 0; k++)
+	{
+	}
+
+	return (k);
+}
+
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: pointer to a char
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
