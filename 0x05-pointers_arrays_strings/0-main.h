5580ac485e7fef2f076fad545666ea48da126051
Erastus Mwaniki <mwanikierastus32@gmail.com>
2022-10-26 17:28:48 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+
+#endif /* MAIN_H */
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h" 
+/**
+ * reset_to_98 - takes a pointer to an int
+ * @n: pointer to an int
+ * Return: Always 0. 
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
