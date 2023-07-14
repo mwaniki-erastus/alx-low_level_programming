78a244f43f633d9707ed78430a987f1bc31c13b1
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-07-13 19:06:20 -0700

    final update on sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n",a);
 	printf("Size of an int: %d byte(s)\n",b);
 	printf("Size of a long int: %d byte(s)\n",c);
-	printf("Size of a long long int:%d byte(s)\n",d);
+	printf("Size of a long long int: %d byte(s)\n",d);
 	printf("Size of a float: %d byte(s)\n",e);
 	return (0);
 }
