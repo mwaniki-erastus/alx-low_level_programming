2d81b9ebe513c0a869187d93bd55acf207ccf01b
Erastus Mwaniki <mwanikierastus32@gmail.com>
2023-07-16 11:19:12 +0000

    greater oe less

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,8 +9,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = n % 10;
-	printf("The last digit of %d is %d ", n, abs(last_digit));
+	int last_digit;
+       	last_digit = n % 10;
+	printf("The last digit of %d is %d ", n, last_digit);
 
 	if (last_digit > 5)
 		printf("and is greater than 5\n");
