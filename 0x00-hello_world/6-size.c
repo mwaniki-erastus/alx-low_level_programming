945516832ad5fe3f44fece37d0bcc2d802539c55
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-07-16 22:16:10 -0700

    Count the number of lines from 1 to identify the line with betty erroe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - Entry point
+ * Return:Always (0) success
+ */
+int main(void)
 {
-	int a,b,c,d,e;
+	int a, b, c, d, e;
 	char i;
 	int j;
 	long int k;
 	long long int l;
-        float m;	
+	float m;
+
 	a = sizeof(i);
 	b = sizeof(j);
 	c = sizeof(k);
 	d = sizeof(l);
 	e = sizeof(m);
-	printf("Size of a char: %d byte(s)\n",a);
-	printf("Size of an int: %d byte(s)\n",b);
-	printf("Size of a long int: %d byte(s)\n",c);
-	printf("Size of a long long int: %d byte(s)\n",d);
-	printf("Size of a float: %d byte(s)\n",e);
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %d byte(s)\n", e);
 	return (0);
 }
