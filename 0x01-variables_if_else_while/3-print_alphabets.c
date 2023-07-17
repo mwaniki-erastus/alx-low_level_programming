90bbd4c4f674c937944f84b32181505e9fa618da
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-07-16 22:46:12 -0700

    Fixing betty errors

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always(0) Success
+ */
 
 int main(void)
 {
 	char l, u;
 
-	for(l=97;l<=122;l++)
+	for (l = 97; l <= 122; l++)
 	{
 		putchar(l);
 	}
-	for(u=65;u<=90;u++)
+	for (u = 65; u <= 90; u++)
 	{
 		putchar(u);
 	}
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Return: Always(0) Suceess
+ */
 int main(void)
 {
 	char l;
 
-	for(l=97;l<=122;l++)
+	for (l = 97; l <= 122; l++)
 	{
 		if (l != 101 && l != 113)
 			putchar(l);
