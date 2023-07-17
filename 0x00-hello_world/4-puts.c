5d6c495b3f6830320657a9159846bd4eb8ed0e53
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-07-16 21:55:32 -0700

    Working on betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-int main(void) 
-{/*
- * main - entry point
- * A program that uses puts to print a string.
- * puts normally prints a new line after printing the cgstring
- * Return - Always 0 (Success)
+/**
+ * main - Entry point
+ * Return:Always (0) Success
  */
-char string[] = "\"Programming is like building a multilingual puzzle";
-puts(string);
-return (0); }		
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}
