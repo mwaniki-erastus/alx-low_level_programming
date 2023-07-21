687b2903106a4a253e4edf53e3467783b4869317
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-07-21 16:27:53 +0000

    This reverts commit 1cdb88793ff5083171a110e82780a1b3df7a201b.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ * main - Entry point.
+ * Tests the positive_or_negative function
+ * Return: Always(0) Success
+ */
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
- * positive_or_negative - checks for that 
- * @n: parameter to be checked.
+ * positive_or_negative - checks for +/-
+ * @i: parameter to bechecked
  */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	if (n < 0)
+	if (i < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
-	else if (n > 0)
+	else if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
+
 }
