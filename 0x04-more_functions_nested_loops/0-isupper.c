e2c004b6bffb66acc5331506f54bc3aa3d54ad3a
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-07-21 19:15:33 +0000

    Check if a character is uppercase or not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _isupper - checks for uppercase
+  * @c: character to be checked
+  * Return: 1(u) or 0(otherwise)
+  */
+
+int _isupper(char c)
+{
+	if isupper(c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-isupper.c - uses the isupper() from <ctype.h> to check if a character is uppercase and returns 1 otherwise o
