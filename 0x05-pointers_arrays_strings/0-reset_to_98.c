92e0a9568f52051237ffa44e98baa2af7a606547
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-07-25 02:04:51 +0000

    Update the value a pointer parameter points to wq

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value given an address of a variable.
+ * @n: takes in a ponter to an integer as a parameter.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+This folder contains projects related to pointers in c programming language
+
+
+0-reset_to_98.c - takes in a pointer to an in parameter and updates the value it points to.
