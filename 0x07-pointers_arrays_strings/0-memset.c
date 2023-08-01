7d4623f044463e25993d3f8c9419bdf5a2d87296
Erastus Mwaniki <mwanikierastus32@gmail.com>
2023-08-01 12:29:44 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  _memset - set a block of memory to a particular value
+ *  @s: pointer to array parameter
+ *  @b: THe value to set
+ *  @n: number of bytes to set from the pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
