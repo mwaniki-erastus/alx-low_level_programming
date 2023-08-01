c56f13bbfcbae83d17c9b2cf1244bbef1416ef1a
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-08-01 16:15:58 +0000

    Final fix for memset. Remove ++ inside codeblock for

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  *  @s: pointer to array parameter
  *  @b: THe value to set
  *  @n: number of bytes to set from the pointer
+ *  Return: pointer to the string
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -13,7 +14,6 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-		i++;
 	}
 
 	return (s);
