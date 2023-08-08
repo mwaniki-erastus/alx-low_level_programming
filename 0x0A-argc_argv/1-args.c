3f9af70520940a22f2a859124a5f767cf8584ef3
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-08-08 02:41:59 +0000

    1-args.c - print thye passed arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: Argument Vector
+ * A program that prints the number or arguments passed
+ * Return: 0 success
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
