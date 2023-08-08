e0df72d0636ea06a216d5eea078064fa2c84a85b
mwaniki-erastus <mwanikierastus32@gmail.com>
2023-08-08 02:31:40 +0000

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * A program that prints its name
+ * Return: ) Success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0-whatsmyname.c - a program that prints out its name followed by a new line. If it is renamed, it will print the name without recompiling.
